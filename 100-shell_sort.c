29bb2fb66c7def9fdd4df8032e5a2fe7bed3d7dd
ahmedbentabet <bentabet841@gmail.com>
2024-02-20 08:29:31 +0000

    Fix task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,48 @@
 #include "sort.h"
 
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ * using the Shell sort algorithm with the Knuth sequence.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, j;
-	int k;
+	size_t gap = 1; /* Initialize the gap with 1 */
+	size_t u;       /* Index variable for iterating through the unsorted part */
+	int s;          /* Index variable for iterating through the sorted part */
+	int key;        /* Variable to store the current element for comparison */
 
+	/* Calculate the initial gap using the Knuth sequence */
 	while (gap < size)
 	{
 		gap = (gap * 3) + 1;
 	}
 	gap = (gap - 1) / 3;
 
+	/* Continue sorting until the gap is greater than 0 */
 	while (gap > 0)
 	{
-		for (j = gap; j < size; j++)
+		u = gap; /* Initialize u with the gap size */
+
+		/* Iterate through the array with the current gap */
+		while (u < size)
 		{
-			for (k = j - gap; k >= 0; k -= gap)
+			key = array[u];
+			s = u - gap;
+
+			/* Compare and swap elements until in the correct order */
+			while (s >= 0 && array[s] > key)
 			{
-				if (array[k + gap] >= array[k])
-					break;
-				else
-				{
-					int temp;
-					temp = array[k + gap];
-					array[k + gap] = array[k];
-					array[k] = temp;
-				}
+				array[s + gap] = array[s];
+				s = s - gap;
 			}
+			array[s + gap] = key;
+
+			u = u + 1; /* Move to the next element in the unsorted part */
 		}
-		gap = (gap - 1) / 3;
-		print_array(array, size);
+
+		gap = (gap - 1) / 3; /* Update the gap using the Knuth sequence */
+		print_array(array, size); /* Print the array after each iteration */
 	}
 }
